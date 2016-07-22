ba7ab692b24ff6e589fc94f75eb2713d8695755d
Ilko <ilia1989@abv.bg>
2016-07-22 18:36:59 +0300

    First Steps with C#

---
+++ 55asterix.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _55asterix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(new string('*', i));
+            }
+        }
+    }
+}
---
+++ AsterixQuadrat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AsterixQuadrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i > 1 && i < n)
+                {
+                    Console.Write("*" + new string(' ', n-2) + "*");
+                    Console.WriteLine();                
+                }
+                else
+                {
+                    Console.WriteLine(new string('*', n) + " ");
+                }
+            }
+           
+        }
+    }
+}

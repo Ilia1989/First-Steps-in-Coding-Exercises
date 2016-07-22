7fab44a629e7799f08145a37e254f6ea8c0ab309
Ilko <ilia1989@abv.bg>
2016-07-22 18:09:17 +0300

    Soft Uni First Lection

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var result = a * b;
+            Console.WriteLine(result);
+        }
+    }
+}
